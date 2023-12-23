83052d778d43ea27ae009956d761419c5f89e906
Milho <jrodmilheiro@gmail.com>
2023-12-23 19:40:17 +0000

    Started permissions plugin

---
+++ Permissions/src/main/java/net/quimmilho/permissions/Permissions.java
@@ -0,0 +1,19 @@
+package net.quimmilho.permissions;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Permissions extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    private void registerEvents() {
+
+    }
+
+    @Override
+    public void onDisable() {}
+
+}
